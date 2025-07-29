7031717752ababeab740ab6141d2c9a91f5162eb
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-07-29 14:41:15 +0530

    Implement signup controller function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,8 @@
 import HttpStatusCodes from "@src/common/HttpStatusCodes";
+import { User } from "@src/models/user.model";
+import { TSignUpBody } from "@src/schemas";
 import type { Request, Response, NextFunction } from "express";
+import bcrypt from 'bcrypt'
 
 export const login = (req: Request, res: Response, next: NextFunction) => {
     res.status(HttpStatusCodes.OK).json({ success: true, message: 'login: TODO!' });
@@ -9,6 +12,27 @@ export const logout = (req: Request, res: Response, next: NextFunction) => {
     res.status(HttpStatusCodes.OK).json({ success: true, message: 'logout: TODO!' })
 }
 
-export const signup = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'signup: TODO!' })
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, name, displayName, email, password } = req.body as TSignUpBody;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const emailExists = !!await User.findOne({ email }).select('-passwordHash').lean();
+
+    if (emailExists) {
+        res.status(HttpStatusCodes.CONFLICT).json({ success: false, message: 'An account already exists with this email!' });
+        return;
+    }
+
+    const usernameExists = !!await User.findOne({ username }).select('-passwordHash').lean();
+
+    if (usernameExists) {
+        res.status(HttpStatusCodes.CONFLICT).json({ success: false, message: 'This username is already taken, try another.' });
+        return;
+    }
+
+    const user = await User.create({ username, name, displayName, email, passwordHash: hashedPassword });
+    const { passwordHash, ...rest } = user.toObject();
+
+    res.status(HttpStatusCodes.OK).json({ success: true, message: 'Created user successfully!', data: { user: rest } })
 }
