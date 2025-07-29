59bc8e95df9111e66e440b09e17365a4319f3856
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-07-29 14:34:55 +0530

    Create User model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,26 @@
+import { IUser, IUserFriend } from "@src/types";
+import { MongooseModel } from "@src/utils/db.utils";
+import mongoose from "mongoose";
+
+const userFriendSchema = new mongoose.Schema<IUserFriend>({
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    status: { type: String, enum: ['confirmed', 'pending'] },
+    direction: { type: mongoose.Schema.Types.Mixed, enum: ['incoming', 'outgoing', null] }
+}, { _id: false });
+
+const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true },
+    displayName: { type: String, required: true },
+    username: { type: String, required: true },
+
+    email: { type: String, required: true },
+
+    passwordHash: { type: String, required: true },
+
+    bio: { type: String, required: false, default: '' },
+    avatarURL: { type: String, required: false, default: '' },
+
+    friends: { type: [userFriendSchema], required: false, default: [] },
+}, { timestamps: true })
+
+export const User = MongooseModel<IUser>('User', userSchema)
