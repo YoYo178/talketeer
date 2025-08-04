da33fae97c050294b338f056fed64460692f95a9
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-04 13:54:32 +0530

    Implement login controller function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,11 +1,46 @@
-import HttpStatusCodes from "@src/common/HttpStatusCodes";
-import { User } from "@src/models/user.model";
-import { TSignUpBody } from "@src/schemas";
-import type { Request, Response, NextFunction } from "express";
 import bcrypt from 'bcrypt'
+import type { Request, Response, NextFunction } from "express";
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'login: TODO!' });
+import HttpStatusCodes from "@src/common/HttpStatusCodes";
+
+import { User } from "@src/models";
+import { TLoginBody, TSignUpBody } from "@src/schemas";
+import { cookieConfig, tokenConfig } from "@src/config";
+import { generateAccessToken, generateRefreshToken } from "@src/utils";
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body as TLoginBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(HttpStatusCodes.NOT_FOUND).json({ success: false, message: 'No user exists with the specified email.' });
+        return;
+    }
+
+    const passwordMatches = !!await bcrypt.compare(password, user.passwordHash);
+
+    if (!passwordMatches) {
+        res.status(HttpStatusCodes.BAD_REQUEST).json({ success: false, message: 'Invalid password!' });
+        return;
+    }
+
+    const refreshToken = generateRefreshToken({ user: { id: user._id.toString(), email: user.email } });
+    const accessToken = generateAccessToken({ user: { id: user._id.toString(), email: user.email, username: user.username } });
+
+    res.cookie('accessToken', accessToken, {
+        ...cookieConfig,
+        maxAge: tokenConfig.accessToken.expiry
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+        ...cookieConfig,
+        maxAge: tokenConfig.refreshToken.expiry
+    });
+
+    const { passwordHash, ...rest } = user.toObject();
+
+    res.status(HttpStatusCodes.OK).json({ success: true, message: 'Logged in successfully!', data: { user: rest } });
 }
 
 export const logout = (req: Request, res: Response, next: NextFunction) => {
