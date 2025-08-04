b4e65278a17a784fd3c2e058a89dd1e12399c9d7
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-04 13:36:59 +0530

    Add JWT types and utils

--- backend/src/types/jwt.types.ts
+++ backend/src/types/jwt.types.ts
@@ -1,3 +1,24 @@
-export type TTokenConfig = {
+export interface TTokenConfig {
     [tokenType: string]: { expiry: number }
+}
+
+export interface TDecodedToken<T> {
+    valid: boolean;
+    expired: boolean;
+    data: T;
+}
+
+export interface TAccessTokenPayload {
+    user: {
+        id: string;
+        email: string;
+        username: string;
+    };
+}
+
+export interface TRefreshTokenPayload {
+    user: {
+        id: string;
+        email: string;
+    };
 }
