6e46bc7b1cc5d2cbaaf873e0cc7c6c74cc6de227
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-04 13:42:52 +0530

    Create socket types

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -3,4 +3,5 @@ export * from './jwt.types';
 export * from './message.types';
 export * from './room.types';
 export * from './schema.types';
+export * from './socket.types';
 export * from './user.types';
---
+++ backend/src/types/socket.types.ts
@@ -0,0 +1,39 @@
+import { ObjectId } from "mongoose";
+import { Server, Socket } from "socket.io";
+import { IUser } from "./user.types";
+
+// TODO
+export interface ServerToClientEvents {
+    roomCreated: (roomId: string, roomName: string, memberCount: number, owner: IUser) => void;
+
+    roomUpdated: (roomId: string, roomName: string, memberCount: number) => void;
+    roomDeleted: (roomId: string, roomName: string) => void;
+
+    memberJoined: (userId: string, userName: string) => void;
+    memberLeft: (userId: string, userName: string) => void;
+
+    memberKicked: (userId: string, userName: string, reason: string) => void;
+    memberBanned: (userId: string, userName: string, reason: string) => void;
+
+    messageReceived: (from: ObjectId, content: string) => void;
+}
+
+// TODO
+export interface ClientToServerEvents {
+}
+
+// TODO
+export interface InterServerEvents {
+    ping: () => void;
+}
+
+export interface SocketData {
+    user: {
+        id: string,
+        username: string,
+        email: string,
+    };
+}
+
+export type TalketeerSocketServer = Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
+export type TalketeerSocket = Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
