bbd646c2f3b3e9ff81114a3ee88db7b5a92393b9
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-17 14:13:30 +0530

    Implement getSelfUser

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,8 +1,13 @@
 import HttpStatusCodes from "@src/common/HttpStatusCodes";
+import { User } from "@src/models";
 import type { Request, Response, NextFunction } from "express";
 
-export const getSelfUser = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'getSelfUser: TODO!' })
+export const getSelfUser = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await User.findById(req.user.id);
+
+    const { passwordHash, ...rest } = user!.toObject(); // Non-null assertion due to auth validation middleware
+
+    res.status(HttpStatusCodes.OK).json({ success: true, data: { user: rest } })
 }
 
 export const getUser = (req: Request, res: Response, next: NextFunction) => {
