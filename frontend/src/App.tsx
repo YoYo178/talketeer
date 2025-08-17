3d3dac8fb0b83c0e5dd863054912906facd73587
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-17 15:02:52 +0530

    Setup redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
 
 import { LandingPage, Auth } from './pages';
 
@@ -8,16 +10,18 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename='/talketeer/'>
-        <RootLayout>
-          <Routes>
-            <Route index element={<LandingPage />} />
-            <Route path='/auth' element={<Auth />} />
-          </Routes>
-        </RootLayout>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter basename='/talketeer/'>
+          <RootLayout>
+            <Routes>
+              <Route index element={<LandingPage />} />
+              <Route path='/auth' element={<Auth />} />
+            </Routes>
+          </RootLayout>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   )
 }
 
