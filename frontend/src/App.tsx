fbbf0479d10c7c8b654a805ee32f2293a99d1fb1
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-19 17:54:01 +0530

    Separate guest and user pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,11 @@ import RootLayout from './layouts/RootLayout';
 import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
 
-import { LandingPage, Auth } from './pages';
+import { UserRoute } from './components/auth/UserRoute.tsx';
+import { GuestRoute } from './components/auth/GuestRoute.tsx';
+
+import { LandingPage, Auth } from './pages/guest';
+import { Chat } from './pages/user';
 
 const queryClient = new QueryClient();
 
@@ -15,8 +19,13 @@ function App() {
         <BrowserRouter basename='/talketeer/'>
           <RootLayout>
             <Routes>
-              <Route index element={<LandingPage />} />
-              <Route path='/auth' element={<Auth />} />
+              <Route element={<UserRoute />}>
+                <Route path='/chat' element={<Chat />} />
+              </Route>
+              <Route element={<GuestRoute />}>
+                <Route index element={<LandingPage />} />
+                <Route path='/auth' element={<Auth />} />
+              </Route>
             </Routes>
           </RootLayout>
         </BrowserRouter>
---
+++ frontend/src/pages/user/Chat.tsx
@@ -0,0 +1,5 @@
+export const Chat = () => {
+  return (
+    <h1>TODO!</h1>
+  )
+}
