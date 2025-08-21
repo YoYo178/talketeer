492d275abdab301516f1c1579e8158b388897368
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-21 18:25:40 +0530

    Update ChatHeader.tsx

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -5,7 +5,7 @@ export const ChatHeader = () => {
     return (
         <div className='flex w-[calc(100%-1.5rem)] bg-(--color-primary-foreground) m-3 mb-0 p-5 rounded-xl'>
             <h1 className='text-2xl font-bold'>Talketeer</h1>
-            <div className="ml-auto flex gap-5">
+            <div className="ml-auto flex gap-3">
                 <ToggleThemeButton />
                 <Button className='ml-auto'>Log out</Button>
             </div>
