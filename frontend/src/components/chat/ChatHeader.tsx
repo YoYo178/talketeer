cf69d153ce9810d108dc6c7a68afc7ff3660e2b9
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-21 17:10:16 +0530

    Update ChatHeader.tsx

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -2,7 +2,7 @@ import { Button } from '../ui/button'
 
 export const ChatHeader = () => {
     return (
-        <div className='flex w-full bg-(--color-primary-foreground) p-5'>
+        <div className='flex w-[calc(100%-1.5rem)] bg-(--color-primary-foreground) m-3 mb-0 p-5 rounded-xl'>
             <h1 className='text-2xl font-bold'>Talketeer</h1>
             <Button className='ml-auto'>Log out</Button>
         </div>
