30b75336cd63c34cf12aae347de22f81458531da
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-21 17:10:26 +0530

    Add theme button in ChatHeader

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -1,10 +1,14 @@
+import ToggleThemeButton from '../scheme/theme-button'
 import { Button } from '../ui/button'
 
 export const ChatHeader = () => {
     return (
         <div className='flex w-[calc(100%-1.5rem)] bg-(--color-primary-foreground) m-3 mb-0 p-5 rounded-xl'>
             <h1 className='text-2xl font-bold'>Talketeer</h1>
-            <Button className='ml-auto'>Log out</Button>
+            <div className="ml-auto flex gap-5">
+                <ToggleThemeButton />
+                <Button className='ml-auto'>Log out</Button>
+            </div>
         </div>
     )
 }
