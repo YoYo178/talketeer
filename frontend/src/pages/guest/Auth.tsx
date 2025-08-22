0498bff921e1e1a2e4816a49ba5b894d966f1e57
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-22 18:45:44 +0530

    Invalidate users queries on login

--- frontend/src/pages/guest/Auth.tsx
+++ frontend/src/pages/guest/Auth.tsx
@@ -1,10 +1,17 @@
 import { AuthForm } from '@/components/auth-form'
+import { useQueryClient } from '@tanstack/react-query'
 
 export const Auth = () => {
+  const queryClient = useQueryClient();
+  
+  const handleSuccessfulLogin = () => {
+    queryClient.invalidateQueries({ queryKey: ['users'] });
+  }
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <AuthForm />
+        <AuthForm onLoginSuccess={handleSuccessfulLogin} />
       </div>
     </div>
   )
