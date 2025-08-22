dcd62fe6cb2c7d81c62734b387d79a43cc015ee1
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-22 18:43:38 +0530

    Add socket sonnection handlers

--- frontend/src/components/auth/GuestRoute.tsx
+++ frontend/src/components/auth/GuestRoute.tsx
@@ -1,13 +1,29 @@
 import { useGetMeQuery } from '@/hooks/network/users/useGetMeQuery';
+import { socket } from '@/socket';
+import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 export const GuestRoute = () => {
-    const { data, isLoading } = useGetMeQuery({ queryKey: ['users', 'me'] });
+    const { data, isLoading, error } = useGetMeQuery({ queryKey: ['users', 'me'] });
+    const [isLoggedIn, setIsLoggedIn] = useState(data?.data?.user);
+
+    useEffect(() => {
+        if (data?.data?.user) {
+            setIsLoggedIn(true);
+            socket.connect();
+        } else {
+            setIsLoggedIn(false);
+            socket.disconnect();
+        }
+    }, [data])
+
+    if (error)
+        return (<Outlet />)
 
     if (isLoading)
         return (<h1>Loading...</h1>)
 
-    if (data?.data?.user)
+    if (isLoggedIn)
         return (<Navigate to='/chat' />)
 
     return (
--- frontend/src/components/auth/UserRoute.tsx
+++ frontend/src/components/auth/UserRoute.tsx
@@ -1,13 +1,29 @@
 import { useGetMeQuery } from '@/hooks/network/users/useGetMeQuery'
+import { socket } from '@/socket';
+import { useState, useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 export const UserRoute = () => {
-    const { data, isLoading } = useGetMeQuery({ queryKey: ['users', 'me'] });
+    const { data, isLoading, error } = useGetMeQuery({ queryKey: ['users', 'me'] });
+    const [isLoggedIn, setIsLoggedIn] = useState(data?.data?.user);
+
+    useEffect(() => {
+        if (data?.data?.user) {
+            setIsLoggedIn(true);
+            socket.connect();
+        } else {
+            setIsLoggedIn(false);
+            socket.disconnect();
+        }
+    }, [data])
+
+    if (error)
+        return (<Navigate to='/auth' />)
 
     if (isLoading)
         return (<h1>Loading...</h1>)
 
-    if (data?.data?.user)
+    if (isLoggedIn)
         return (<Outlet />)
 
     return (
