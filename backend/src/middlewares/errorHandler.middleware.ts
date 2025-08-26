f26a6b9d9d2b9840e2a3fe512375e8728a02b185
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-26 12:23:21 +0530

    Add logging in error handler

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -2,5 +2,6 @@ import HttpStatusCodes from "@src/common/HttpStatusCodes"
 import type { Request, Response, NextFunction } from "express"
 
 export const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err)
     res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: 'An error occured in the server. Please try again.' })
 }
