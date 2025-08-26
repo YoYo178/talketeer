a5ba6a840dff9ef15f326b7884bc70a9b62faefe
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-26 17:09:06 +0530

    Update 'disconnect' event
    The disconnect event now makes the user leave the room they are in, and updates the DB accordingly.
    Finally, it emits a 'roomUpdated' event that others can use to get the latest room details.

--- backend/src/sockets/handlers/general.sockets.ts
+++ backend/src/sockets/handlers/general.sockets.ts
@@ -1,8 +1,28 @@
+import { Room, User } from '@src/models';
 import { TalketeerSocket, TalketeerSocketServer } from '@src/types/socket.types';
 
 export function registerGeneralHandlers(io: TalketeerSocketServer, socket: TalketeerSocket) {
-    socket.on('disconnect', (reason, description) => {
+    socket.on('disconnect', async (reason, description) => {
         console.log(`${socket.data.user.username} disconnected`);
+
+        const user = await User.findById(socket.data.user.id);
+        if (!user?.room) return;
+
+        await User.updateOne(
+            { _id: user._id },
+            {
+                room: null
+            }
+        )
+
+        await Room.updateOne(
+            { _id: user.room },
+            {
+                $pull: { members: { user: socket.data.user.id } }
+            }
+        );
+
+        io.emit('roomUpdated', user.room.toString());
     });
 
     socket.on('disconnecting', (reason, description) => {
