a423941e91dd91b39c34cf177af059d11c2f0f2f
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-26 12:37:01 +0530

    Implement log out functionality

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -1,13 +1,26 @@
+import { useLogoutMutation } from '@/hooks/network/auth/useLogoutMutation'
 import ToggleThemeButton from '../scheme/theme-button'
 import { Button } from '../ui/button'
+import { useQueryClient } from '@tanstack/react-query';
 
 export const ChatHeader = () => {
+    const queryClient = useQueryClient();
+
+    const logoutMutation = useLogoutMutation({ queryKey: ['auth'] });
+
+    const handleLogout = async () => {
+        await logoutMutation.mutateAsync({});
+
+        if (logoutMutation.isSuccess)
+            queryClient.invalidateQueries({ queryKey: ['users'] });
+    }
+
     return (
         <div className='flex w-[calc(100%-1.5rem)] bg-(--color-primary-foreground) m-3 mb-0 p-5 rounded-xl'>
             <h1 className='text-2xl font-bold'>Talketeer</h1>
             <div className="ml-auto flex gap-3">
                 <ToggleThemeButton />
-                <Button className='ml-auto'>Log out</Button>
+                <Button className='ml-auto' onClick={handleLogout}>Log out</Button>
             </div>
         </div>
     )
