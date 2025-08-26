450f0eb1536d4bc5bf9df3059eddffd0730c9b34
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-26 17:04:17 +0530

    Update message model

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -5,9 +5,6 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema<IMessage>({
     content: { type: String, required: true },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    seen: { type: Boolean, required: false },
-    timestamp: { type: Number, required: true } // TODO: remove in favor of createdAt and updatedAt?
 }, { timestamps: true })
 
 export const Message = MongooseModel<IMessage>('Message', messageSchema)
--- backend/src/types/message.types.ts
+++ backend/src/types/message.types.ts
@@ -8,19 +8,9 @@ export interface IMessage {
     /** The user who sent the message */
     sender: DBRef<IUser>;
 
-    /** The user to receive the message */
-    recipient: DBRef<IUser>;
-
     /** The content of the message */
     content: string;
 
-    /** The message creation timestamp
-     * @todo Remove in favor of createdAt and updatedAt?
-    */
-    timestamp: number;
-
-    /** Whether the user has seen the message or not
-     * @todo Not sure if this will be implemented, and even if it will be, the type will probably be changed later
-    */
-    seen: boolean;
+    updatedAt: number;
+    createdAt: number;
 }
