40b9694b749760d367591ac58f0dafc2548e08b3
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-31 18:40:40 +0530

    Update message.sockets.ts

--- frontend/src/sockets/message.sockets.ts
+++ frontend/src/sockets/message.sockets.ts
@@ -3,15 +3,15 @@ import type { Socket } from "socket.io-client";
 export function startListeningMessageEvents(socket: Socket) {
     stopListeningMessageEvents(socket);
 
-    socket.on('newMessage', (userId: string, message: string) => {
+    socket.on('newMessage', (roomId: string, userId: string, message: string, rawMessage?: IMessage) => {
         console.log(`Received new message from ${userId}: ${message}`);
     });
 
-    socket.on('messageEdited', (userId: string, oldMessage: string, newMessage: string) => {
+    socket.on('messageEdited', (roomId: string, userId: string, oldMessage: string, newMessage: string) => {
         console.log(`${userId} edited their message from '${oldMessage}' to '${newMessage}'`);
     });
 
-    socket.on('messageDeleted', (userId: string, deletedBy: string, message: string) => {
+    socket.on('messageDeleted', (roomId: string, userId: string, deletedBy: string, message: string) => {
         if (userId === deletedBy)
             return console.log(`${deletedBy} has deleted their message: ${message}`)
 
