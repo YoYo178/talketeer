0eff98748b322dbe82fa68a66abcef71106a644c
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-31 18:35:10 +0530

    Limit messages returned with rooms

--- backend/src/controllers/rooms.controller.ts
+++ backend/src/controllers/rooms.controller.ts
@@ -4,19 +4,36 @@ import { TRoomIdParams } from "@src/schemas";
 import type { Request, Response, NextFunction } from "express";
 
 export const getAllRooms = async (req: Request, res: Response, next: NextFunction) => {
-    const rooms = await Room.find({}).populate('messages');
+    const rooms = await Room.find({})
+        .populate({
+            path: 'messages',
+            options: { sort: { createdAt: -1 } },
+            perDocumentLimit: 20
+        });
+
+    rooms.forEach(room => {
+        room.messages = room.messages.reverse();
+    })
+
     res.status(HttpStatusCodes.OK).json({ success: true, data: { rooms } })
 }
 
 export const getRoomById = async (req: Request, res: Response, next: NextFunction) => {
     const { roomId } = req.params as TRoomIdParams;
 
-    const room = await Room.findById(roomId).populate('messages');
+    const room = await Room.findById(roomId)
+        .populate({
+            path: 'messages',
+            options: { sort: { createdAt: -1 } },
+            perDocumentLimit: 20
+        });
 
     if (!room) {
         res.status(HttpStatusCodes.NOT_FOUND).json({ success: false, message: 'Room not found' })
         return;
     }
+
+    room.messages = room.messages.reverse();
 
     res.status(HttpStatusCodes.OK).json({ success: true, data: { room: room.toObject() } })
 }
