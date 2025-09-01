cadea01a035fd89c041c443e0b92de778a99468f
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-01 13:10:24 +0530

    Clear stale DB data on startup

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,10 +1,29 @@
 import ENV from "@src/common/ENV"
+import { Room, User } from "@src/models";
 import mongoose from "mongoose"
+
+async function clearStaleData() {
+    try {
+        console.info('Attempting to clear stale data from the database...')
+
+        // Clear all members in all rooms
+        await Room.updateMany({}, { $set: { members: [] } });
+
+        // Clear users' room references
+        await User.updateMany({}, { $unset: { room: '' } });
+
+        console.info('Cleared stale data from the database.')
+    } catch (e) {
+        console.error('An error occured while attempting to clear stale data.')
+        console.error(e?.message || e)
+    }
+}
 
 export const connectDB = async () => {
     try {
         await mongoose.connect(ENV.MongodbUri);
         console.info("Connected to MongoDB successfully.")
+        clearStaleData();
     } catch (error) {
         console.error("Couldn't connect to MongoDB!\nExtra details: " + error?.message)
     }
