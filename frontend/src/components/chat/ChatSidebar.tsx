52f348cb56de10e1cb2aee59829b8c5cdad374e5
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-03 19:04:09 +0530

    Update ChatSidebar.tsx

--- frontend/src/components/chat/ChatSidebar.tsx
+++ frontend/src/components/chat/ChatSidebar.tsx
@@ -1,11 +1,11 @@
 import { useMemo, useState } from 'react'
 import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
 import { useGetRoomsQuery } from '@/hooks/network/rooms/useGetRoomsQuery'
 import type { IRoom } from '@/types/room.types'
 import type { FC } from 'react'
 import { RoomList } from './rooms/RoomList'
-import { HousePlus, Plus } from 'lucide-react'
+import { CreateRoomDialog } from './rooms/dialogs/CreateRoomDialog'
+import { JoinRoomDialog } from './rooms/dialogs/JoinRoomDialog'
 
 interface ChatSidebarProps {
     onSelectRoomId: (roomId: string | null) => void;
@@ -27,8 +27,8 @@ export const ChatSidebar: FC<ChatSidebarProps> = ({ onSelectRoomId, selectedRoom
     return (
         <div className='flex flex-col gap-3 w-72 p-3 rounded-xl bg-card'>
             <div className='flex gap-2 max-w-full'>
-                <Button className='flex-1 whitespace-nowrap'><Plus className='size-5' />New room</Button> {/* TODO: functionality */}
-                <Button className='flex-1 whitespace-nowrap'><HousePlus className='size-5' />Join room</Button> {/* TODO: functionality */}
+                <CreateRoomDialog />
+                <JoinRoomDialog />
             </div>
 
             <Input placeholder='Search rooms…' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
