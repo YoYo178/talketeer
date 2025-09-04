e62c357b7ee969d2c8f3ab99602b11cbc3e26392
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-04 12:54:25 +0530

    Reduce number of system rooms

--- backend/src/config/rooms.config.ts
+++ backend/src/config/rooms.config.ts
@@ -1,6 +1,6 @@
 import { IRoom } from "@src/types";
 
-export const MAX_SYSTEM_ROOMS = 15;
+export const MAX_SYSTEM_ROOMS = 3;
 
 export const DEFAULT_SYSTEM_ROOM_CONFIG: Omit<IRoom, '_id' | 'owner' | 'createdAt' | 'updatedAt'> = {
     code: '',
