ab9aa146969fa0148e1c59533bc7e9da85248a8a
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-04 12:42:42 +0530

    Add styling for private rooms

--- frontend/src/components/chat/rooms/RoomEntry.tsx
+++ frontend/src/components/chat/rooms/RoomEntry.tsx
@@ -4,6 +4,7 @@ import { startListeningRoomEvents, stopListeningRoomEvents } from '@/sockets/roo
 import type { IRoom } from '@/types/room.types'
 import { useQueryClient } from '@tanstack/react-query';
 import type { FC } from 'react'
+import { Lock } from 'lucide-react'
 
 interface RoomEntryProps {
     room: IRoom;
@@ -20,7 +21,7 @@ export const RoomEntry: FC<RoomEntryProps> = ({ room: localRoom, onSelectRoomId,
         }
     );
 
-    const room = data?.data?.room ?? localRoom;
+    const room: IRoom = data?.data?.room ?? localRoom;
 
     const handleRoomJoin = async () => {
         if (!room || room._id === selectedRoomId)
@@ -51,17 +52,21 @@ export const RoomEntry: FC<RoomEntryProps> = ({ room: localRoom, onSelectRoomId,
 
     return (
         <button
-            key={room?._id}
+            key={room._id}
             onClick={handleRoomJoin}
-            className={`w-full text-left p-3 hover:bg-accent/40 cursor-pointer ${selectedRoomId === room?._id ? 'bg-accent/60' : ''}`}
+            className={`w-full text-left p-3 hover:bg-accent/40 cursor-pointer ${selectedRoomId === room._id ? 'bg-accent/60' : ''}`}
         >
             <div className='flex items-center gap-2'>
-                <div className='font-medium'>{room?.name}</div>
-                {(room?.isSystemGenerated) && (
+                {room.visibility === 'private' && (
+                    <Lock className='text-muted-foreground' size={16} />
+                )}
+                <div className='font-medium'>{room.name}</div>
+
+                {room.isSystemGenerated && (
                     <span className='text-[10px] px-1.5 py-0.5 rounded-md bg-secondary-foreground text-primary-foreground font-bold'>SYSTEM</span>
                 )}
             </div>
-            <div className='text-xs text-muted-foreground'>Members: {room?.members?.length}/{room?.memberLimit}</div>
+            <div className='text-xs text-muted-foreground'>Members: {room.members?.length}/{room.memberLimit}</div>
         </button>
     )
 }
