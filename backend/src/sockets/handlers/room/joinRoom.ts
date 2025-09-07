ea1226dad60aa97f6862b1f9784b753165f21ff6
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-07 12:18:22 +0530

    Update joinRoom.ts
    Add join by code method

--- backend/src/sockets/handlers/room/joinRoom.ts
+++ backend/src/sockets/handlers/room/joinRoom.ts
@@ -1,10 +1,29 @@
-import { joinRoom } from "@src/services/room.service";
-import { AckFunc, TalketeerSocket, TalketeerSocketServer } from "@src/types/socket.types";
+import { getRoomByCode, joinRoom } from "@src/services/room.service";
+import { ClientToServerEvents, TalketeerSocket, TalketeerSocketServer } from "@src/types/socket.types";
 
-export const getJoinRoomEventCallback = (io: TalketeerSocketServer, socket: TalketeerSocket) => {
-    return async (roomId: string, ack: AckFunc) => {
+export const getJoinRoomEventCallback = (io: TalketeerSocketServer, socket: TalketeerSocket): ClientToServerEvents['joinRoom'] => {
+    return async ({ method, data }, ack) => {
         try {
             const userId = socket.data.user.id;
+            let roomId: string | null = null;
+
+            switch (method) {
+                case "id":
+                    roomId = data;
+                    break;
+
+                case "code":
+                    const roomByCode = await getRoomByCode(data);
+                    if (!roomByCode) throw new Error("Room not found");
+                    roomId = roomByCode._id.toString();
+                    break;
+
+                default:
+                    throw new Error('Unknown join method');
+            }
+
+            if (!roomId)
+                throw new Error('Room ID not found');
 
             // Handle database join process
             await joinRoom(userId, roomId);
@@ -13,17 +32,18 @@ export const getJoinRoomEventCallback = (io: TalketeerSocketServer, socket: Talk
             socket.join(roomId);
             console.log(`${socket.data.user.username} joined room ${roomId}`);
 
-            ack(true);
-
             // Broadcast the member join event to everyone in this room
             io.to(roomId).emit('memberJoined', userId);
 
             // Let other people (even ones not in the room) refetch the latest room details
             io.emit('roomUpdated', roomId);
-            ack(true);
+            ack({ success: true, data: roomId });
         } catch (err) {
             console.error("Error joining room:", err);
-            ack(false, err?.message || 'Unknown error');
+            ack({
+                success: false,
+                error: err?.message || 'Unknown error'
+            });
         }
     }
 }
