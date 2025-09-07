c2272e62ccbd8990c5642c8495d5cd2e1d6aa3a0
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-07 20:57:05 +0530

    Remove room controllers in favor of sockets

--- backend/src/controllers/rooms.controller.ts
+++ backend/src/controllers/rooms.controller.ts
@@ -44,27 +44,3 @@ export const getRoomById = async (req: Request, res: Response, next: NextFunctio
 
     res.status(HttpStatusCodes.OK).json({ success: true, data: { room: sanitizeRoomObj(room, req.user.id) } })
 }
-
-export const createRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'createRoom: TODO!' })
-}
-
-export const deleteRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'deleteRoom: TODO!' })
-}
-
-export const joinRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'joinRoom: TODO!' })
-}
-
-export const leaveRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'leaveRoom: TODO!' })
-}
-
-export const kickUserFromRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'kickUserFromRoom: TODO!' })
-}
-
-export const banUserFromRoom = (req: Request, res: Response, next: NextFunction) => {
-    res.status(HttpStatusCodes.OK).json({ success: true, message: 'banUserFromRoom: TODO!' })
-}
--- backend/src/routes/rooms.routes.ts
+++ backend/src/routes/rooms.routes.ts
@@ -1,22 +1,13 @@
 import { Router } from "express";
 
 import { validate } from "@src/middlewares";
-import { createRoomSchema, roomIdParamsSchema, roomMemberParamsSchema } from "@src/schemas";
+import { roomIdParamsSchema } from "@src/schemas";
 
-import { getAllRooms, banUserFromRoom, createRoom, getRoomById, joinRoom, kickUserFromRoom, leaveRoom, deleteRoom } from "@src/controllers";
+import { getAllRooms, getRoomById } from "@src/controllers";
 
 const RoomsRouter = Router();
 
 RoomsRouter.get('/', getAllRooms);
 RoomsRouter.get('/:roomId', validate({ params: roomIdParamsSchema }), getRoomById);
-
-RoomsRouter.post('/', validate({ body: createRoomSchema }), createRoom);
-RoomsRouter.delete('/:roomId', validate({ params: roomIdParamsSchema }), deleteRoom);
-
-RoomsRouter.post('/:roomId/members', validate({ params: roomIdParamsSchema }), joinRoom);
-RoomsRouter.delete('/:roomId/members/me', validate({ params: roomIdParamsSchema }), leaveRoom);
-
-RoomsRouter.delete('/:roomId/members/:userId', validate({ params: roomMemberParamsSchema }), kickUserFromRoom);
-RoomsRouter.patch('/:roomId/members/:userId', validate({ params: roomMemberParamsSchema }), banUserFromRoom);
 
 export default RoomsRouter;
