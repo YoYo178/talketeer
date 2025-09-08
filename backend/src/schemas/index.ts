c9c94ceabf6ad673ddaadfb834319ba3b57e8fe0
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-08 14:29:32 +0530

    Update schemas barrel file

--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -2,4 +2,5 @@ export * from './auth.schema';
 export * from './friends.schema';
 export * from './rooms.schema';
 export * from './users.schema';
+export * from './rooms.schema';
 export * from './messages.schema';
