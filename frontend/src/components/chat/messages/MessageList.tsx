77620555ce5c04af81e38c8ad934f7bb67761c92
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-11 16:44:55 +0530

    Update MessageList.tsx
    Updated message list to fetch messages via infinite scrolling method.
    
    Note - **This change currently breaks message sending functionality**

--- frontend/src/components/chat/messages/MessageList.tsx
+++ frontend/src/components/chat/messages/MessageList.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useLayoutEffect, useMemo, useRef, type FC } from 'react'
+import { useCallback, useLayoutEffect, useMemo, useRef, type FC } from 'react'
 import { MessageBlock, MessageBlockSkeleton } from './MessageBlock'
 import type { IMessage } from '@/types/message.types';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import React from 'react';
+import { useGetMessagesQuery } from '@/hooks/network/messages/useGetMessagesQuery';
+import { Separator } from '@/components/ui/separator';
 
 const MessageListSkeleton = () => {
     const alignArr: ('start' | 'end')[] = ['start', 'end']
@@ -20,68 +22,82 @@ const MessageListSkeleton = () => {
 }
 
 interface MessageListProps {
-    messages: IMessage[];
-    selectedRoomId?: string | null;
-    areMessagesLoading: boolean;
+    selectedRoomId: string;
 }
 
-export const MessageList: FC<MessageListProps> = ({ messages, selectedRoomId, areMessagesLoading }) => {
+export const MessageList: FC<MessageListProps> = ({ selectedRoomId }) => {
+    const chatEndRef = useRef<HTMLDivElement>(null);
     const isAtBottom = useRef(false);
 
-    const chatEndRef = useRef<HTMLDivElement>(null);
+    const { data, isLoading, fetchNextPage, hasNextPage } = useGetMessagesQuery({
+        queryKey: ['messages', selectedRoomId],
+        queryParams: { roomId: selectedRoomId }
+    })
+    const messagePages = data?.pages || [];
 
-    const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const handleScroll = useCallback((e: React.UIEvent<HTMLDivElement>) => {
         const element = e.target as HTMLDivElement;
 
         isAtBottom.current = element.scrollHeight - element.scrollTop - element.clientHeight < 50;
-    }
+
+        if (element.scrollTop === 0 && hasNextPage) {
+            const prevHeight = element.scrollHeight;
+
+            fetchNextPage?.().then(() => {
+                requestAnimationFrame(() => {
+                    const newHeight = element.scrollHeight;
+                    element.scrollTop = newHeight - prevHeight;
+                })
+            });
+        }
+    }, [hasNextPage, fetchNextPage])
+
+    const messageElements = useMemo(() => {
+        return messagePages.flatMap(page => {
+            const messages = (page.data?.messages || []).reverse();
+            if (!messages.length) return [];
+
+            const messageGroups: IMessage[][] = [];
+            let currentGroup: IMessage[] = [];
+
+            messages.forEach((message, index) => {
+                const previousMessage = messages[index - 1];
+
+                if (previousMessage && previousMessage.sender === message.sender) {
+                    currentGroup.push(message);
+                } else {
+                    if (currentGroup.length)
+                        messageGroups.push(currentGroup);
+
+                    currentGroup = [message];
+                }
+            })
+
+            if (currentGroup.length)
+                messageGroups.push(currentGroup);
+
+            return messageGroups.map(group => (
+                <MessageBlock key={group[0]._id} senderId={group[0].sender} messages={group} />
+            ))
+        }).reverse()
+    }, [messagePages])
 
     const scrollToBottom = () => {
         if (chatEndRef.current)
-            chatEndRef.current.scrollIntoView({ behavior: 'auto' })
+            chatEndRef.current.scrollIntoView({ behavior: 'auto' });
     };
 
+    // This might look awkward, but is intended
+    // this hook must only run when messageElements
+    // transitions from empty to non-empty
     useLayoutEffect(() => {
         scrollToBottom();
-    }, [selectedRoomId])
-
-    useEffect(() => {
-        if (isAtBottom.current)
-            scrollToBottom();
-    }, [messages])
-
-    const messageElements = useMemo(() => {
-        if (!messages?.length) return [];
-
-        const messageGroups = [];
-
-        let currentGroup: IMessage[] = [];
-        for (let i = 0; i < messages.length; i++) {
-            const message = messages[i];
-            const previousMessage = messages[i - 1];
-
-            if (previousMessage && previousMessage.sender === message.sender) {
-                currentGroup.push(message);
-            } else {
-                if (currentGroup.length)
-                    messageGroups.push(currentGroup);
-
-                currentGroup = [message];
-            }
-        }
-
-        if (currentGroup.length)
-            messageGroups.push(currentGroup);
-
-        return messageGroups.map(group => (
-            <MessageBlock key={group[0]._id} senderId={group[0].sender} messages={group} />
-        ))
-    }, [messages])
+    }, [messageElements.length === 0])
 
     if (!messageElements.length) {
         return (
             <div className='flex-1 flex items-center justify-center overflow-hidden m-6'>
-                {areMessagesLoading ? (
+                {isLoading ? (
                     <MessageListSkeleton />
                 ) : (
                     <div className='text-center'>
@@ -95,6 +111,15 @@ export const MessageList: FC<MessageListProps> = ({ messages, selectedRoomId, ar
 
     return (
         <ScrollArea className='flex flex-col flex-1 p-4 pt-0 pb-0 overflow-y-auto overflow-x-hidden' onScroll={handleScroll}>
+            {messageElements.length > 0 && !hasNextPage && (
+                <div className="flex items-center my-2">
+                    <Separator className='flex-1' />
+                    <span className="mx-2 bg-card px-2 text-muted-foreground">
+                        You've reached the end of this chatroom!
+                    </span>
+                    <Separator className='flex-1' />
+                </div>
+            )}
             {messageElements}
             <div ref={chatEndRef} />
         </ScrollArea>
