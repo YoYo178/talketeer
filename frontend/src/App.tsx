3d50c8f5cb6b7efb2f75d74fa8b0ca5f028b1913
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-12 15:17:49 +0530

    Remove Redux (Unused)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GuestLayout } from './layouts/GuestLayout.tsx';
-import { Provider } from 'react-redux'
-import { store } from './store/index.ts'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
 
 import { UserRoute } from './components/auth/UserRoute.tsx';
@@ -16,23 +14,21 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <ErrorBoundary>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter basename='/talketeer/'>
-            <Routes>
-              <Route element={<UserRoute />}>
-                <Route path='/chat' element={<Chat />} />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter basename='/talketeer/'>
+          <Routes>
+            <Route element={<UserRoute />}>
+              <Route path='/chat' element={<Chat />} />
+            </Route>
+            <Route element={<GuestRoute />}>
+              <Route element={<GuestLayout />}>
+                <Route index element={<LandingPage />} />
+                <Route path='/auth' element={<Auth />} />
               </Route>
-              <Route element={<GuestRoute />}>
-                <Route element={<GuestLayout />}>
-                  <Route index element={<LandingPage />} />
-                  <Route path='/auth' element={<Auth />} />
-                </Route>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </QueryClientProvider>
-      </Provider>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </ErrorBoundary>
   )
 }
