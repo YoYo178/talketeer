df326b0345f7de21e54f492ae1694700d7b64e20
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-14 18:26:40 +0530

    Update frontend middleware layouts

--- frontend/src/components/auth/GuestRoute.tsx
+++ frontend/src/components/auth/GuestRoute.tsx
@@ -1,16 +1,10 @@
 import { useGetMeQuery } from '@/hooks/network/users/useGetMeQuery';
-import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 export const GuestRoute = () => {
     const { data, isLoading, error } = useGetMeQuery({ queryKey: ['users', 'me'] });
     const me = data?.data?.user;
-
-    const [isLoggedIn, setIsLoggedIn] = useState(!!me?._id);
-
-    useEffect(() => {
-        setIsLoggedIn(!!me?._id);
-    }, [me?._id]);
+    const isLoggedIn = !!me?._id;
 
     if (error)
         return (<Outlet />)
