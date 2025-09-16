ec5bf80b031e9b3953371f397275422368a5a8ab
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-16 15:48:08 +0530

    Add media queries in NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,12 +7,16 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useMe } from '@/hooks/network/users/useGetMeQuery';
 import type { IUser } from '@/types/user.types';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { useMediaQuery } from '@/hooks/ui/useMediaQuery';
 
 export const NavBar = () => {
     const queryClient = useQueryClient();
     const me: IUser | undefined = useMe();
 
     const logoutMutation = useLogoutMutation({ queryKey: ['auth'] });
+
+    const hideTitle = useMediaQuery('(max-width: 590px)')
+    const hideLogoutText = useMediaQuery('(max-width: 465px)')
 
     const handleLogout = async () => {
         try {
@@ -30,16 +34,15 @@ export const NavBar = () => {
 
             <div className='flex gap-2 items-center'>
                 <MessagesSquare className='size-8' />
-                <h1 className='text-2xl font-bold'>Talketeer</h1>
+                {!hideTitle && <h1 className='text-2xl font-bold'>Talketeer</h1>}
             </div>
 
             {/* temporary, until i come up with a proper layout and a proper place */}
-            <div className='flex flex-1 justify-center gap-4 relative'>
+            <div className='flex flex-1 mx-4 justify-center gap-4 relative'>
                 <Avatar className='outline-1 outline-muted-foreground size-12'>
                     <AvatarImage src={me?.avatarURL} />
                     <AvatarFallback>{me?.displayName.split(' ').map(str => str[0].toUpperCase()).join('')}</AvatarFallback>
                 </Avatar>
-
                 <div className='flex flex-col mt-[0.1rem]'>
                     <p className='text-primary text-l font-bold'>{me?.displayName}</p>
                     <p className='text-muted-foreground text-sm -translate-y-1'>@{me?.username}</p>
@@ -56,7 +59,10 @@ export const NavBar = () => {
                         <p>Toggle theme</p>
                     </TooltipContent>
                 </Tooltip>
-                <Button className='ml-auto' onClick={handleLogout}><LogOut />Log out</Button>
+                <Button className='ml-auto' onClick={handleLogout}>
+                    <LogOut />
+                    {!hideLogoutText && <>Log out</>}
+                </Button>
             </div>
 
         </div>
