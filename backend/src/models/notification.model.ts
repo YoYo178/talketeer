bcf4231f59e7ff7b1f428147e6dea60ce65f38ea
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-19 19:31:23 +0530

    Create Notification model

---
+++ backend/src/models/notification.model.ts
@@ -0,0 +1,15 @@
+import { INotification } from "@src/types";
+import { MongooseModel } from "@src/utils";
+import mongoose from "mongoose";
+
+const notificationTypes = ['friend-request', 'friend-new', 'friend-delete', 'room-ban', 'room-delete', 'room-invite', 'room-kick', 'system', 'unknown']
+
+const notificationSchema = new mongoose.Schema<INotification>({
+    content: { type: String, required: true },
+    type: { type: String, required: true, enum: notificationTypes, default: 'unknown' },
+
+    // TTL target field
+    createdAt: { type: Date, expires: '7d', default: Date.now },
+}, { timestamps: true });
+
+export const Notification = MongooseModel<INotification>('Notification', notificationSchema);
