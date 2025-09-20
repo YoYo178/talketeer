05b884a6121a6fd03d7c7b617de2ba6e2b282c3c
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-09-20 13:38:08 +0530

    Create notifications schema

---
+++ backend/src/schemas/notifications.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+import { mongooseObjectId } from '../utils';
+
+export const notificationIdParams = z.object({
+    notificationId: mongooseObjectId
+})
+
+export type TNotificationIdParams = z.infer<typeof notificationIdParams>
