4767c7736e123b4736370b5828b6b317bac59469
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-02 15:59:04 +0530

    Update users.controller.ts

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,29 +1,35 @@
 import HttpStatusCodes from "@src/common/HttpStatusCodes";
 import { User } from "@src/models";
 import { TUserIdParams } from "@src/schemas";
-import { IPublicUser } from "@src/types";
 import { APIError } from "@src/utils/api.utils";
 import type { Request, Response, NextFunction } from "express";
 
 export const getMe = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await User.findById(req.user.id);
-
-    const { passwordHash, ...rest } = user!.toObject(); // Non-null assertion due to auth validation middleware
-
-    res.status(HttpStatusCodes.OK).json({ success: true, data: { user: rest } })
-}
-
-export const getUser = async (req: Request, res: Response, next: NextFunction) => {
-    const { userId } = req.params as TUserIdParams;
-
-    const user = await User.findById(userId);
+    const user = await User.findById(req.user.id).select('-passwordHash').lean().exec();
 
     if (!user)
         throw new APIError('User not found', HttpStatusCodes.NOT_FOUND);
 
-    const { name, email, passwordHash, friends, notifications, room, updatedAt, ...rest } = user.toObject();
+    res.status(HttpStatusCodes.OK).json({ success: true, data: { user } })
+}
 
-    const publicUserObj: IPublicUser = { ...rest };
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req.params as TUserIdParams;
+    const user = await User.findById(userId)
+        .select(`
+            -passwordHash
+            -name
+            -email
+            -friends
+            -notifications
+            -room
+            -updatedAt
+        `)
+        .lean()
+        .exec();
 
-    res.status(HttpStatusCodes.OK).json({ success: true, data: { user: publicUserObj } })
+    if (!user)
+        throw new APIError('User not found', HttpStatusCodes.NOT_FOUND);
+
+    res.status(HttpStatusCodes.OK).json({ success: true, data: { user } });
 }
