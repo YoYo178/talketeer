7f3755e056b1fdb5c9adc8e8a1c605ff001d713c
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-10 21:55:05 +0530

    Create api.config.ts
    Add default rate limit options

---
+++ backend/src/config/api.config.ts
@@ -0,0 +1,9 @@
+import { Options } from 'express-rate-limit';
+
+export const DEFAULT_RATE_LIMIT_OPTIONS: Partial<Options> = {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: { message: "Too many attempts from this IP, Please try again later" },
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+}
