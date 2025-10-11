7dac4333515908b924b6d50f4913458b233074e6
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-11 15:22:44 +0530

    Update mail.templates.ts
    Add separate subject and body properties

--- backend/src/templates/mail.templates.ts
+++ backend/src/templates/mail.templates.ts
@@ -1,13 +1,19 @@
-export const verificationMailTemplate = `
-    <h2>Verify Your Account</h2>
-    <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
-    <a href="{link}">Verify via Link</a>
-    <p>OTP: <b>{code}</b></p>
-`
+export const verificationMailTemplate = {
+    subject: `Welcome to {appName} | Verify your email`,
+    body: `
+        <h2>Verify Your Account</h2>
+        <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
+        <a href="{link}">Verify via Link</a>
+        <p>OTP: <b>{code}</b></p>
+    `
+}
 
-export const passwordResetMailTemplate = `
-    <h2>Reset Your Password</h2>
-    <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
-    <a href="{link}">Verify via Link</a>
-    <p>OTP: <b>{code}</b></p>
-`
+export const passwordResetMailTemplate = {
+    subject: `{appName} | Reset your password`,
+    body: `
+        <h2>Reset Your Password</h2>
+        <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
+        <a href="{link}">Verify via Link</a>
+        <p>OTP: <b>{code}</b></p>
+    `
+}
