cd4e03143ce295a49953078056dd7e43921d5a59
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-11 13:17:11 +0530

    Update mail.templates.ts
    Fix typo

--- backend/src/templates/mail.templates.ts
+++ backend/src/templates/mail.templates.ts
@@ -2,12 +2,12 @@ export const verificationMailTemplate = `
     <h2>Verify Your Account</h2>
     <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
     <a href="{link}">Verify via Link</a>
-    <p>OTP: <b>{otp}</b></p>
+    <p>OTP: <b>{code}</b></p>
 `
 
 export const passwordResetMailTemplate = `
     <h2>Reset Your Password</h2>
     <p>Click the link below or enter the OTP (Expires in 10 minutes):</p>
     <a href="{link}">Verify via Link</a>
-    <p>OTP: <b>{otp}</b></p>
+    <p>OTP: <b>{code}</b></p>
 `
