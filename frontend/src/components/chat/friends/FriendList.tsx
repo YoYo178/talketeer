bf1c1d68a73c7399d7266d168e5b57f90ddabf49
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-29 20:30:52 +0530

    Improve no friends found text

--- frontend/src/components/chat/friends/FriendList.tsx
+++ frontend/src/components/chat/friends/FriendList.tsx
@@ -11,7 +11,10 @@ export const FriendList: FC<FriendListProps> = ({ friends }) => {
     return (
         <ScrollArea className='flex-1 overflow-y-auto rounded-md border border-border/40'>
             {friends.length <= 0 ? (
-                <div className='p-3 text-sm text-muted-foreground'>No friends found</div>
+                <div className='p-3 flex-col w-full text-center'>
+                    <p className='text-sm text-muted-foreground font-semibold'>No friends found</p>
+                    <p className='text-sm text-muted-foreground'>Add some friends to get the party going!</p>
+                </div>
             ) : (
                 <div className='flex flex-col gap-2'>
                     {friends.map(friendObj => <FriendEntry key={friendObj.userId} friendObject={friendObj} />)}
