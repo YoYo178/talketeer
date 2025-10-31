e079ce47e2b07e692c9d9bcf611f262cf4f85ef4
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-31 15:23:23 +0530

    Fix Socket URL
    Turns out I had to use the path parameter instead of manually changing the URL.

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 import { SOCKET_SERVER_URL } from './config/api.config';
 import type { TalketeerSocket } from './types/socket.types';
 
-const serverURL = import.meta.env.PROD ? SOCKET_SERVER_URL + '/talketeer' : SOCKET_SERVER_URL
+const socketPath = import.meta.env.PROD ? '/talketeer/socket.io/' : '/socket.io/'
 
-export const socket: TalketeerSocket = io(serverURL, { withCredentials: true, autoConnect: false });
+export const socket: TalketeerSocket = io(SOCKET_SERVER_URL, { withCredentials: true, autoConnect: false, path: socketPath });
