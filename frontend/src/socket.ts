3aac6c6627edac82f0cad5b9b451701032e51aa5
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-10-31 14:51:56 +0530

    Fix Socket URL

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,6 @@ import { io } from 'socket.io-client';
 import { SOCKET_SERVER_URL } from './config/api.config';
 import type { TalketeerSocket } from './types/socket.types';
 
-export const socket: TalketeerSocket = io(SOCKET_SERVER_URL, { withCredentials: true, autoConnect: false });
+const serverURL = import.meta.env.PROD ? SOCKET_SERVER_URL + '/talketeer' : SOCKET_SERVER_URL
+
+export const socket: TalketeerSocket = io(serverURL, { withCredentials: true, autoConnect: false });
