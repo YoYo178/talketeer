679f8b841b47ce8b4654400a18878956718629a7
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-12-17 16:25:13 +0530

    Update server URLs

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 import { SOCKET_SERVER_URL } from './config/api.config';
 import type { TalketeerSocket } from './types/socket.types';
 
-export const socket: TalketeerSocket = io(SOCKET_SERVER_URL, { withCredentials: true, autoConnect: false });
+export const socket: TalketeerSocket = io(SOCKET_SERVER_URL, { withCredentials: true, autoConnect: false, path: '/talketeer/socket.io/' }) as TalketeerSocket;
