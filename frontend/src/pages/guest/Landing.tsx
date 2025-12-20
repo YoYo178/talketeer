5c5945155250bae7460a8f25ba9284289e39d8b9
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-12-20 18:25:09 +0530

    Add footer in landing page

--- frontend/src/pages/guest/Landing.tsx
+++ frontend/src/pages/guest/Landing.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { getFooter } from '@/utils/misc.utils';
 import { useNavigate } from 'react-router-dom';
 
 export const LandingPage = () => {
@@ -6,7 +7,7 @@ export const LandingPage = () => {
 
     return (
         <div className='min-h-screen flex flex-col items-center justify-center bg-background text-foreground px-4'>
-            <h1 className='text-4xl md:text-5xl font-bold text-center mb-4'>
+            <h1 className='text-4xl md:text-5xl font-bold text-center mb-4 mt-auto'>
                 Welcome to Talketeer
             </h1>
             <p className='text-muted-foreground text-center max-w-md mb-8'>
@@ -15,6 +16,9 @@ export const LandingPage = () => {
             <Button size='lg' className='px-8 text-lg cursor-pointer' onClick={() => navigate('/auth')}>
                 Get Started
             </Button>
+            <div className='mt-auto mb-8 flex flex-col gap-1 text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4'>
+                {getFooter()}
+            </div>
         </div>
     );
 };
