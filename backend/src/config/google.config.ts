0c4a524e5eff3532b7b164858d6ed82bcf94b7f7
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-01-21 15:45:25 +0530

    Update Google OAuth2 strategy config

--- backend/src/config/google.config.ts
+++ backend/src/config/google.config.ts
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+
 import ENV from '@src/common/ENV';
 import { User } from '@src/models';
 
@@ -7,24 +8,30 @@ passport.use(new GoogleStrategy({
   clientID: ENV.GoogleClientId,
   clientSecret: ENV.GoogleClientSecret,
   callbackURL: `${ENV.BackendOrigin}/api/auth/google/callback`,
-}, async (accessToken, refreshToken, profile, done) => {
+}, async (_accessToken, _refreshToken, profile, done) => {
   try {
-    const email = profile?.emails && profile.emails.length > 0 ? profile.emails[0].value : undefined;
-    if (!email) {
-      return done(new Error('No email found in Google profile'), false);
-    }
-    let user = await User.findOne({ email });
-    if (!user) {
-      user = await User.create({
-        name: profile.displayName,
-        email: email,
-        username: email.split('@')[0],
-        passwordHash: '',
-        isVerified: true,
-        verifiedAt: new Date(),
-      });
-    }
-    return done(null, user);
+    const googleEmail = profile.emails?.[0]?.value;
+
+    if (!googleEmail)
+      return done(new Error('There was an error while fetching your email from Google.'), false);
+
+    let user = await User.findOne({ email: googleEmail });
+
+    // If the user does not exist, create a new one
+    user ??= await User.create({
+      name: profile.displayName,
+      displayName: profile.displayName,
+      email: googleEmail,
+      username: profile.username ?? googleEmail.split('@')[0],
+      passwordHash: '', // TODO: Add change password functionality to allow user to set their password in case of an OAuth2 registration.
+
+      isVerified: true, // OAuth2 speciality, no mail verification needed
+      verifiedAt: new Date(), // OAuth2 speciality, no mail verification needed
+    });
+
+    const { _id, username, email } = user.toObject();
+
+    return done(null, { id: _id.toString(), username, email });
   } catch (err) {
     return done(err, false);
   }
