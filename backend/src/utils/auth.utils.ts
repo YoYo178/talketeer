f168688b4a00b4e4a4b5c36f83e39f1b6b5427cb
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-01-21 15:45:06 +0530

    Create auth.utils.ts

---
+++ backend/src/utils/auth.utils.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import { cookieConfig, tokenConfig } from "@src/config";
+import { generateRefreshToken, generateAccessToken } from "./jwt.utils";
+
+/* Generates JWT tokens and issues HttpOnly cookies */
+export function issueCookies(res: Response, userId: string, email: string, username: string) {
+    const refreshToken = generateRefreshToken({ user: { id: userId, email } });
+    const accessToken = generateAccessToken({ user: { id: userId, email, username } });
+
+    res.cookie('accessToken', accessToken, {
+        ...cookieConfig,
+        maxAge: tokenConfig.accessToken.expiry,
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+        ...cookieConfig,
+        maxAge: tokenConfig.refreshToken.expiry,
+    });
+}
